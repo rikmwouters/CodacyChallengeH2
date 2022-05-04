99b0ab551d04afa9521ec58d118e1b53645815ec
rikmwouters <rikmwouters@gmail.com>
2022-05-04 14:54:18 +0100

    First commit consisting of the Spring Initializr files.

---
+++ src/main/java/com/Codacy/ChallengeH2/ChallengeH2Application.java
@@ -0,0 +1,13 @@
+package com.Codacy.ChallengeH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeH2Application.class, args);
+	}
+
+}
