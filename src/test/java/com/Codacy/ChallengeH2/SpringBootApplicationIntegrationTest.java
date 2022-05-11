0d6843a1140cf1099b9f4f872aa7b51088b29a6e
Rik Wouters <rikmwouters@gmail.com>
2022-05-11 14:59:43 +0100

    Addition of an integration test, which provides further information regarding possible persistence problems.

---
+++ src/test/java/com/Codacy/ChallengeH2/SpringBootApplicationIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.Codacy.ChallengeH2;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.Charset;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ChallengeH2Application.class)
+@WebAppConfiguration
+public class SpringBootApplicationIntegrationTest {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setupMockMvc() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void givenRequestHasBeenMade_whenMeetsAllOfGivenConditions_thenCorrect()
+            throws Exception {
+        MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+                MediaType.APPLICATION_JSON.getSubtype());
+        mockMvc.perform(MockMvcRequestBuilders.get("/")).
+                andExpect(MockMvcResultMatchers.status().isOk()).
+                andExpect(MockMvcResultMatchers.content().contentType(contentType)).
+                andExpect(jsonPath("$", hasSize(2)));
+    }
+}
